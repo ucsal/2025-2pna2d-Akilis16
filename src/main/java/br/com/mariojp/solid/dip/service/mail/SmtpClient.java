a91e7269101a79766da5b28cebde8d8cb50d193a
Akilis16 <gabrielsebastiao16072005@gmail.com>
2025-09-01 20:31:44 -0300

    Implements DIP in code + packeges

--- src/main/java/br/com/mariojp/solid/dip/service/mail/SmtpClient.java
+++ src/main/java/br/com/mariojp/solid/dip/service/mail/SmtpClient.java
@@ -1,11 +1,20 @@
-package br.com.mariojp.solid.dip;
+package br.com.mariojp.solid.dip.service.mail;
 
-public class SmtpClient {
+import br.com.mariojp.solid.dip.service.MailService;
+
+public class SmtpClient implements MailService{
+	
+	public SmtpClient() {
+		System.setProperty("SMTP_AVAILABLE", "true");
+	}
+	
+	@Override
     public void send(String to, String subject, String body){
         if(!"true".equalsIgnoreCase(System.getProperty("SMTP_AVAILABLE"))) {
             throw new IllegalStateException("SMTP indisponível");
         }
         if(to == null || !to.contains("@")) throw new IllegalArgumentException("email inválido");
         // envia ...
+        System.out.println("Email sent success with SMTP!");
     }
 }
